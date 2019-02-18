f7d7d7aaf2c27e4c5b866bc3a132900a86d27b34
libailb <libai@xiaomi.com>
2019-02-18 17:42:21 +0800

    上传到git

---
+++ src/main/java/com/example/test/mavendeploy/Entity/User.java
@@ -0,0 +1,9 @@
+package com.example.test.mavendeploy.Entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
---
+++ src/main/java/com/example/test/mavendeploy/MavendeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.mavendeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavendeployApplication.class, args);
+    }
+
+}
